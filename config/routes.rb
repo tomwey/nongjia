294f523ff1df87e7ff859b1386ba3038796eb3e0
Tomwey <tomwey@163.com>
2016-03-10 23:08:25 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   
   ######################### 微信公众平台开发 ###########################
-  scope path: '/wechat', via: :post, defaults: { format: 'xml' } do
-    root 'weixin/home#welcome', constraints: Weixin::Router.new(type: 'text', content: 'Hello2BizUser')
-  end
-  
+  post '/wechat' => 'weixin/home#welcome', defaults: { format: 'xml' }, constraints: Weixin::Router.new(type: 'text', content: 'Hello2BizUser')
   get "/wechat" => 'weixin/home#show'
   get "/fetch_access_token" => 'weixin/home#fetch_access_token'
   
