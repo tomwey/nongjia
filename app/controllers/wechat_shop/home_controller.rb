eb6c1711dc5c4a245305a7a7a08fa62640428ae3
Tomwey <tomwey@163.com>
2016-03-19 21:34:19 +0800

    refactor codes

---
+++ app/controllers/wechat_shop/home_controller.rb
@@ -0,0 +1,6 @@
+class WechatShop::HomeController < WechatShop::ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  
+    
+end
