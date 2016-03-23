dd276a43009def2cbaaf21187f5b1c0b410b6f2f
tomwey <tomwey@163.com>
2016-03-23 13:23:27 +0800

    do shipment

--- app/controllers/wechat_shop/shipments_controller.rb
+++ app/controllers/wechat_shop/shipments_controller.rb
@@ -3,8 +3,11 @@ class WechatShop::ShipmentsController < WechatShop::ApplicationController
   before_filter :require_user
   before_filter :check_user
   
+  skip_before_filter  :verify_authenticity_token, only: [:destroy]
+  
   def index
     @shipments = current_user.shipments.order('id DESC')
+    @from = params[:from]
   end
   
   def new
@@ -12,11 +15,29 @@ class WechatShop::ShipmentsController < WechatShop::ApplicationController
   end
   
   def create
-    
+    @shipment = current_user.shipments.new(shipment_params)
+    if @shipment.save
+      flash[:notice] = '新增地址成功'
+      redirect_to wechat_shop_shipments_path
+    else
+      render :new
+    end
   end
   
   def destroy
-    
+    if current_user.current_shipment_id.present? and 
+      params[:id].present? and params[:id].to_i == current_user.current_shipment_id.to_i
+      flash[:error] = '您不能删除默认收货地址'
+    else
+      @shipment = current_user.shipments.find(params[:id])
+      @shipment.destroy
+    end
+    redirect_to wechat_shop_shipments_path
+  end
+  
+  private
+  def shipment_params
+    params.require(:shipment).permit(:name, :mobile, :region, :address)
   end
   
 end
