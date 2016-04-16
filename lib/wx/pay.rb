dc98fd42f3241435a558ff921c28986fa2475a37
Tomwey <tomwey@163.com>
2016-04-16 18:33:26 +0800

    ddd

--- lib/wx/pay.rb
+++ lib/wx/pay.rb
@@ -25,6 +25,7 @@ module WX
       xml = params.to_xml(root: 'xml', skip_instruct: true, dasherize: false)
       # puts xml
       result = RestClient.post 'https://api.mch.weixin.qq.com/pay/unifiedorder', xml, { :content_type => :xml }
+      puts result
       pay_result = Hash.from_xml(result)[:xml]
       return pay_result
     end
