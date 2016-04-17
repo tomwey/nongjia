15718437432cb71c658a05510b862c27d32d4c07
Tomwey <tomwey@163.com>
2016-04-17 14:47:28 +0800

    pay notify

--- app/controllers/wechat_shop/orders_controller.rb
+++ app/controllers/wechat_shop/orders_controller.rb
@@ -118,16 +118,26 @@ class WechatShop::OrdersController < WechatShop::ApplicationController
   end
   
   def wx_pay_notify
+    @output = {
+      return_code: '',
+      return_msg: 'OK',
+    }
+    
     result = params['xml']
     if result and result['return_code'] == 'SUCCESS' and WX::Pay.notify_verify?(result)
       # 支付成功，更改订单状态
       order = Order.find_by(order_no: result['out_trade_no'])
       order.pay unless order.blank?
-      
+      @output[:return_code] = 'SUCCESS'
     else
       # 支付失败
+      @output[:return_code] = 'FAIL'
     end
-    # TODO: 返回结果给微信服务器
+    
+    respond_to do |format|
+      format.xml { render xml: @output.to_xml(root: 'xml', skip_instruct: true, dasherize: false) }
+    end
+    
   end
   
   private
