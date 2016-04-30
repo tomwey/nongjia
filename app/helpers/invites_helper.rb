c25475d5f50f187c726c349d859c5625b94f339e
Tomwey <tomwey@163.com>
2016-04-30 22:32:06 +0800

    update some css

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -3,9 +3,9 @@ module InvitesHelper
     return '' if invite.blank?
     
     if invite.icon.blank?
-      image_tag 'wechat_shop/default_invite_share_icon.png', class: 'img-circle'
+      image_tag 'wechat_shop/default_invite_share_icon.png'#, class: 'img-responsive'
     else
-      image_tag invite.icon.url(:big), class: 'img-circle'
+      image_tag invite.icon.url(:big)#, class: 'img-responsive'
     end
     
   end
