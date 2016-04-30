06e9e97f78c87f6456454146d7f6dcf70940aa4a
Tomwey <tomwey@163.com>
2016-05-01 00:30:37 +0800

    update css

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,7 @@
 class Shipment < ActiveRecord::Base
   belongs_to :user, inverse_of: :shipments
   
-  validates :name, :mobile, :address, presence: true
+  validates :name, :mobile, :address, :region, presence: true
   validates :mobile, format: { with: /\A1[3|4|5|7|8][0-9]\d{4,8}\z/, message: "号码不正确" },
                      length: { is: 11 }
 end
