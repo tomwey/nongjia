be22b9c2192b33b69ba0a7571e086b10d04afc97
Tomwey <tomwey@163.com>
2016-05-05 22:06:02 +0800

    do share

--- app/controllers/wechat_shop/products_controller.rb
+++ app/controllers/wechat_shop/products_controller.rb
@@ -4,7 +4,7 @@ class WechatShop::ProductsController < WechatShop::ApplicationController
     @product = Product.find(params[:id])
     @page_title = @product.title
     
-    @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.domain}/wx-shop/products/#{@product.id}", @product.title, '')
+    @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.upload_url}/wx-shop/products/#{@product.id}", @product.title, @product.intro || '巴适得很，欢迎选购！！！')
     
     fresh_when(etag: [@product, @page_title])
   end
