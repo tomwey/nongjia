3455739c5e94e9457ccb6dbf88244c3f3e04b4a5
Tomwey <tomwey@163.com>
2016-05-06 21:07:46 +0800

    update product wx share

--- app/controllers/wechat_shop/products_controller.rb
+++ app/controllers/wechat_shop/products_controller.rb
@@ -4,7 +4,7 @@ class WechatShop::ProductsController < WechatShop::ApplicationController
     @product = Product.find(params[:id])
     @page_title = @product.title
     
-    @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.upload_url}/wx-shop/products/#{@product.id}", @product.title, @product.intro || '巴适得很，欢迎选购！！！')
+    @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.upload_url}/wx-shop/products/#{@product.id}", '农家风味商城', @product.title)
     
     fresh_when(etag: [@product, @page_title])
   end
