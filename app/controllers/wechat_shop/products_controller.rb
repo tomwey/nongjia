9d1fa774de244e7b53050de6b7ef7ed3f93dd9e9
Tomwey <tomwey@163.com>
2016-05-08 19:17:55 +0800

    fixed share bug

--- app/controllers/wechat_shop/products_controller.rb
+++ app/controllers/wechat_shop/products_controller.rb
@@ -4,7 +4,7 @@ class WechatShop::ProductsController < WechatShop::ApplicationController
     @product = Product.find(params[:id])
     @page_title = @product.title
     
-    @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.upload_url}/wx-shop/products/#{@product.id}", '农家风味商城', @product.title)
+    @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.upload_url}/wx-shop/products/#{@product.id}", "农家风味商城", "测试内容")
     
     fresh_when(etag: [@product, @page_title])
   end
