e107ca1fcc3c5528c2083e02cdaca9c39f10e32c
Tomwey <tomwey@163.com>
2016-05-24 00:06:53 +0800

    remove wx share product

--- app/controllers/wechat_shop/products_controller.rb
+++ app/controllers/wechat_shop/products_controller.rb
@@ -4,7 +4,7 @@ class WechatShop::ProductsController < WechatShop::ApplicationController
     @product = Product.find(params[:id])
     @page_title = @product.title
     
-    @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.upload_url}/wx-shop/products/#{@product.id}", "农家风味商城", @page_title)
+    # @wx_share = WXShare.new(@product.images.first.url(:small), "#{Setting.upload_url}/wx-shop/products/#{@product.id}", "农家风味商城", @page_title)
     
     fresh_when(etag: [@product])
   end
