2c1e55f6c581a4327de327382073ce45e9eb922c
Tomwey <tomwey@163.com>
2016-06-06 23:50:11 +0800

    ccc

--- app/controllers/wechat_shop/home_controller.rb
+++ app/controllers/wechat_shop/home_controller.rb
@@ -9,21 +9,23 @@ class WechatShop::HomeController < WechatShop::ApplicationController
     
     # 加载限时活动
     @sale = FlashSale.order('id desc').limit(1).first
-    @product = Product.where(id: @sale.product_ids).order('id desc').first
+    if not @sale.blank?
+      @product = Product.where(id: @sale.product_ids).order('id desc').first
     
-    @start = (@sale.begin_time - Time.now).to_i
-    @end   = (@sale.end_time - Time.now).to_i
+      @start = (@sale.begin_time - Time.now).to_i
+      @end   = (@sale.end_time - Time.now).to_i
     
-    @duration = @start > 0 ? @start : @end
+      @duration = @start > 0 ? @start : @end
     
-    @hour = @duration / 3600
-    @min  = ( @duration - 3600 * @hour ) / 60
-    @sec  = @duration - @hour * 3600 - @min * 60;
+      @hour = @duration / 3600
+      @min  = ( @duration - 3600 * @hour ) / 60
+      @sec  = @duration - @hour * 3600 - @min * 60;
+    end
     
     # 加载腰风
     @ad = Ad.order('sort desc, id desc').limit(1).first
     
-    # fresh_when(etag: [@products, @banners, @current, @ad, @sale, @product])
+    fresh_when(etag: [@products, @banners, @current, @ad, @sale, @product])
   end
     
 end
